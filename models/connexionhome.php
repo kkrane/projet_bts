a4b2b01707cae4203908a40cd17f53f839833411
MrReYz <jerome.chaure@orange.fr>
2017-03-07 17:31:16 +0100

    Changement connexion

--- models/connexionhome.php
+++ models/connexionhome.php
@@ -3,22 +3,25 @@
 require 'models/connexion.php';
     
 if(isset($_POST['submit']))
-	{
+{
 		$login = $_POST['login'];
 		$mdp = ($_POST['mdp']);
 		
 		$requete = $bdd->query("SELECT * FROM user
 		                        WHERE mdp ='".$mdp."' 
 								AND identifiant ='".$login."'");
-		if($reponse = $requete->fetch())
-		{
-			$_SESSION['connecte'] = true;
-			$_SESSION['identifiant'] = $reponse['identifiant'];
-			$_SESSION['mdp'] = $reponse['mdp'];
-            header('location:formation');
+        if($requete->rowCount() == 1 )
+    {
+            $reponse = $requete->fetch();
+            $_SESSION['connecte'] = true;
+            $_SESSION['identifiant'] = $login;
+            $_SESSION['credit'] = $reponse['credit'];
+            $_SESSION['nbr_jour'] = $reponse['nbr_jour'];
+            $_SESSION['id_s'] = $reponse['id_s'];
+            $_SESSION['chef'] = $reponse['chef'];
+            header('location:accueil');
 			die();
-		}
-        else
-            echo"Login ou Mot de Passe incorrect";
-	}
+    }
+        else alert: "Login ou Mot de Passe incorrect";  
+}
 ?>
