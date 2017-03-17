2c7680d5206aa2661a44105e903531962b3b2cb3
kkrane <cloarec.j95@gmail.com>
2017-03-17 14:30:28 +0100

    accueil chef pour controler les formations

--- models/accueil.php
+++ models/accueil.php
@@ -44,4 +44,43 @@ function getFormationRefuse()
     return $req;
 };
 
+function getFormationDemande()
+{
+    $bdd = $GLOBALS['bdd'];
+    $sql = "SELECT * FROM formation f, user u, formation_suivi fs WHERE f.id_f = fs.id_f AND fs.id_s = u.id_s AND fs.valide = 0";
+    $req = $bdd->query($sql);
+    
+    if(!$req)
+    {
+        echo 'requête déféctueuse';
+    }
+    return $req;
+}
+
+function getFormationAccepte2()
+{
+    $bdd = $GLOBALS['bdd'];
+    $sql = "SELECT * FROM formation f, user u, formation_suivi fs WHERE f.id_f = fs.id_f AND fs.id_s = u.id_s AND fs.valide = 1";
+    $req = $bdd->query($sql);
+    
+    if(!$req)
+    {
+        echo 'requête déféctueuse';
+    }
+    return $req;
+}
+
+function getFormationRefuse2()
+{
+    $bdd = $GLOBALS['bdd'];
+    $sql = "SELECT * FROM formation f, user u, formation_suivi fs WHERE f.id_f = fs.id_f AND fs.id_s = u.id_s AND fs.valide = 2";
+    $req = $bdd->query($sql);
+    
+    if(!$req)
+    {
+        echo 'requête déféctueuse';
+    }
+    return $req;
+}
+
 ?>
--- views/accueil.php
+++ views/accueil.php
@@ -139,32 +139,106 @@
             ?>
             <div class="container">
                 <div class="row">
-                    <h3 style="color: #28a7e9" class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">FORMATION REFUSE</h3>
+                    <h3 style="color: #28a7e9" class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">Demande en attente</h3>
                     <table class="table">
                       <thead class="thead-inverse">
                         <tr>
-                          <th>Formation</th>
-                          <th>Contenu</th>
-                          <th>Date</th>
-                          <th>Durée</th>
-                          <th>Coût</th>
-                          <th>Lieu</th>
+                          <th style="text-align: center;">Salarié</th>
+                          <th style="text-align: center;">Formation</th>
+                          <th style="text-align: center;">Durée</th>
+                          <th style="text-align: center;">Valider</th>
+                          <th style="text-align: center;">Refuser</th>
                         </tr>
                       </thead>
                       <tbody>
                           
               <?php
-                $req = getFormationEnAttente();
+                $req = getFormationDemande();
                 while($donnee = $req->fetch())
                 {
               ?>
                         <tr>
-                          <td scope="row"><?php echo $donnee['titre'] ?></td>
-                          <td scope="row"><?php echo $donnee['contenu'] ?></td>
-                          <td scope="row"><?php echo $donnee['date_debut'] ?></td>
-                          <td scope="row"><?php echo $donnee['duree'] ?></td>
-                          <td scope="row"><?php echo $donnee['cout'] ?></td>
-                          <td scope="row"><?php echo $donnee['lieu'] ?></td>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['nom'] ?></td>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['titre'] ?></td>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['duree'] ?> jours</td>
+                          <td style="text-align: center;" scope="row"><img style="width: 20px; height: 20px;" src="images/valider.ico"></img></td>
+                          <td style="text-align: center;" scope="row"><img style="width: 20px; height: 20px; text-align: center;" src="images/refuser.ico"</td>
+                        </tr>
+
+                      
+            <?php
+                    }
+                
+            ?>
+            
+     
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="row">
+                    <h3 style="color: #28a7e9" class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">Demande accepté </h3>
+                    <table class="table">
+                      <thead class="thead-inverse">
+                        <tr>
+                          <th style="text-align: center;">Salarié</th>
+                          <th style="text-align: center;">Formation</th>
+                          <th style="text-align: center;">Durée</th>
+                          <th style="text-align: center;">Annuler</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                          
+              <?php
+                $req = getFormationAccepte2();
+                while($donnee = $req->fetch())
+                {
+              ?>
+                        <tr>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['nom'] ?></td>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['titre'] ?></td>
+                          <td style="text-align: center;" scope="row"><?php echo  $donnee['duree'] ?> jours</td>
+                          <td style="text-align: center;" scope="row"><img style="width: 20px; height: 20px; text-align: center;" src="images/refuser.ico"</td>
+                        </tr>
+
+                      
+            <?php
+                    }
+                
+            ?>
+            
+     
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="row">
+                    <h3 style="color: #28a7e9" class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">Demande refusé</h3>
+                    <table class="table">
+                      <thead class="thead-inverse">
+                        <tr>
+                          <th style="text-align: center;">Salarié</th>
+                          <th style="text-align: center;">Formation</th>
+                          <th style="text-align: center;">Durée</th>
+                          <th style="text-align: center;">Annuler</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                          
+              <?php
+                $req = getFormationRefuse2();
+                while($donnee = $req->fetch())
+                {
+              ?>
+                        <tr>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['nom'] ?></td>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['titre'] ?></td>
+                          <td style="text-align: center;" scope="row"><?php echo $donnee['duree'] ?> jours</td>
+                          <td style="text-align: center;" scope="row"><img style="width: 20px; height: 20px; text-align: center;" src="images/refuser.ico"</td>
                         </tr>
 
                       
