d147c451939c7dcbefe2508b40f4c2fa4a6e2b11
kkrane <cloarec.j95@gmail.com>
2017-06-13 19:36:17 +0200

    Annulation de formation

---
+++ controllers/annulation.php
@@ -0,0 +1,32 @@
+<?php
+
+require "models/accueil.php";
+
+session_start();
+
+if (isset($_POST['id_formation']))
+{
+        
+    $bdd = $GLOBALS['bdd'];
+    
+    if(!$bdd)
+    {
+        echo "Pas de connexion à la base de donnée";
+    }
+    
+    $id_f = $_POST['id_formation'];
+    $id_s = $_POST['id_user'];
+    
+    /*$req = UpdateDemandeEnAttente2();
+    $req = bindParam(':id_f', $id_f);
+    $req = bindParam(':id_s', $id_s);
+    $req->execute();*/
+    $bdd = $GLOBALS['bdd'];
+    $sql = "UPDATE formation_suivi SET `valide` = 0 WHERE id_f = '".$id_f."' AND id_s = '".$id_s."'";
+    $req = $bdd->query($sql);
+        
+}
+
+
+  
+die();
