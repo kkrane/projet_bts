c4f3330abefe1bfd6324bf45dd481d9e5c88459a
kkrane <cloarec.j95@gmail.com>
2017-06-13 19:42:09 +0200

    Mise a jour (credit, nb_jour)

--- controllers/inscription.php
+++ controllers/inscription.php
@@ -1,6 +1,6 @@
 <?php
 
-require "models/accueil.php";
+require "models/formation.php";
 
 session_start();
 
@@ -16,12 +16,16 @@ if (isset($_POST['id_formation']))
     
     $id_f = $_POST['id_formation'];
     $id_s = $_POST['id_user'];
+    $cout = $_POST['cout'];
+    $duree = $_POST['duree'];
     
     
     $bdd = $GLOBALS['bdd'];
     $sql = "INSERT INTO formation_suivi (id_f, id_s, valide) VALUES('".$id_f."','".$id_s."', 0)";
     $req = $bdd->query($sql);
-        
+    
+    $sql1 = "UPDATE user SET nbr_jour = nbr_jour - '$duree', credit = credit - '$cout' WHERE id_s = '".$id_s."'";
+    $req1 = $bdd->query($sql1);    
 }
 
 
